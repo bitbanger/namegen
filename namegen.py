fdd5d4d6f7c12183d632108e2341b36bf3147b9d
bitbanger <lxl5734@rit.edu>
2014-11-17 18:43:59 -0500

    Randomize second seed letter as a vowel + cleanup

--- namegen.py
+++ namegen.py
@@ -33,37 +33,44 @@ def next_letter(letter1, letter2, trigrams):
 			return letter
 
 
-trigrams = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+def main():
+	if len(sys.argv) < 3:
+		print "USAGE: python %s <corpus_file> <#names>" % (sys.argv[0])
+		quit()
 
-if len(sys.argv) < 3:
-	print "USAGE: python %s <corpus_file> <#names>" % (sys.argv[0])
-	quit()
+	trigrams = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
-# Open corpus & retrieve names
-corpus_file = sys.argv[1]
-names = []
-with open(corpus_file, 'r') as f:
-	names = [string.strip(x).lower() for x in f.readlines()]
+	# Open corpus & retrieve names
+	corpus_file = sys.argv[1]
+	names = []
+	with open(corpus_file, 'r') as f:
+		names = [string.strip(x).lower() for x in f.readlines()]
 
-# Build trigram frequencies
-for name in names:
-	for c in range(len(name)-2):
-		trigrams[name[c]][name[c+1]][name[c+2]] += 1
+	# Build trigram frequencies
+	for name in names:
+		for c in range(len(name)-2):
+			trigrams[name[c]][name[c+1]][name[c+2]] += 1
 
-num_names = int(sys.argv[2])
-for i in range(num_names):
-	name = ""
-	rname = choice(names)
-	name = rname[0] + rname[1]
-	tries = 0
-	max_tries = 100
-	mean_len, stdev_len = name_len_mean_stdev(names)
-	exp_len = gauss(mean_len, stdev_len)
-	while True:
-		nl = next_letter(name[-2], name[-1], trigrams)
-		if not nl or len(name) > exp_len:
-			print name
-			break
+	# Make names!
+	num_names = 0
+	try:
+		num_names = int(sys.argv[2])
+	except:
+		print "expected an integer value for #names; got \"%s\"" % (sys.argv[2])
+		quit()
+	for i in range(num_names):
+		name = ""
+		rname = choice(names)
+		name = rname[0] + choice([c for c in trigrams[rname[0]].keys() if c in "aeiou"])
+		mean_len, stdev_len = name_len_mean_stdev(names)
+		exp_len = gauss(mean_len, stdev_len)
+		while True:
+			nl = next_letter(name[-2], name[-1], trigrams)
+			if not nl or len(name) > exp_len:
+				print name
+				break
 
-		tries = 0
-		name = name + nl
+			name = name + nl
+
+if __name__ == "__main__":
+	main()
